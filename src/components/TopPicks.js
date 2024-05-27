43a3df5b93de36ba0a23aaf012069d801cc011bb
Javad Ghanbari <javadghanbari.put@gmail.com>
2024-05-27 02:35:08 -0700

    Debuging

--- src/components/TopPicks.js
+++ src/components/TopPicks.js
@@ -6,7 +6,7 @@ import '@splidejs/react-splide/css';
 const TopPicks = () => {
     return (
         <>
-        <h1 className='text-orange-500 font-bold text-2xl text-center py-2'>Top Picks</h1> 
+        <h1 className='hidden lg:block text-orange-500 font-bold text-2xl text-center py-2'>Top Picks</h1> 
         <div className='hidden lg:flex max-w-[1520px] m-auto py-2 px-2'>
             <Splide options={{perPage:4, gap:"0.5rem", drag:'free', arrows:false}}  >
             {
