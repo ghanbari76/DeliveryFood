c15761fda201bdd3d492630febee8ac48edf8736
Javad Ghanbari <javadghanbari.put@gmail.com>
2024-05-27 02:41:40 -0700

    Making the quickApp component responsive

--- src/components/Delivery.js
+++ src/components/Delivery.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Delivery = () => {
     return (
-        <div className='w-full bg-white py-16 px-4'>
+        <div className=' w-full bg-white py-6 px-4'>
             <h3 className='text-orange-500 font-bold text-2xl text-center'>Quick Delivery App</h3>
-            <div className='w-[1240px] mx-auto grid md:grid-cols-2 '>
+            <div className='w-fit mx-auto grid md:grid-cols-2 '>
                 <img className='w-[550px] mx-auto my-4' src='https://res.cloudinary.com/ehizeex-shop/image/upload/v1672676822/NetflixApp/FC_two_phones.6ec9a842f905769677f9_m91off.webp' alt='Delivery App' />
                 <div className='flex flex-col justify-center'>
                     <p className='text-[#00df9a] font-bold'>Get The App</p>
@@ -17,9 +17,6 @@ const Delivery = () => {
                         adipisicing elit. Voluptatum molestiae delectus culpa hic assumenda,
                         voluptate reprehenderit dolore autem cum ullam sed odit
                         perspiciatis. Doloribus quos velit, eveniet ex deserunt fuga? Lorem
-                        ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-                        molestiae delectus culpa hic assumenda, voluptate reprehenderit
-                        dolore autem cum ullam sed odit perspiciatis. Doloribus quos velit,
                         eveniet ex deserunt fuga?                        
                     </p>
                     <button className='bg-blue-600 hover:bg-blue-700 border-none text-white w-[150px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3'> Get Started</button>
